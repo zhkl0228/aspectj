af49d876dd46bc6eb933ebf7463b4881f986ff58
Banny <zhkl0228@qq.com>
2018-08-28 22:58:11 +0800

    Use asm-6.2.1.jar, support jdk 10, 11

--- bridge/src/org/aspectj/bridge/Version.java
+++ bridge/src/org/aspectj/bridge/Version.java
@@ -37,7 +37,7 @@ public class Version {
       * Time text set by build script using SIMPLE_DATE_FORMAT.
       * (if DEVELOPMENT version, invalid)
       */
-    public static final String time_text = "星期日 八月 26, 2018 at 02:59:02 GMT";
+    public static final String time_text = "星期二 八月 28, 2018 at 14:56:16 GMT";
 
     /** 
       * time in seconds-since-... format, used by programmatic clients.
